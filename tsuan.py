6da59f26654c7ab87f20050b0726092129702935
薛丞宏 <ihcaoe@gmail.com>
2018-09-27 13:52:06 +0800

    做好頭一版

--- tsuan.py
+++ tsuan.py
@@ -1,4 +1,6 @@
 import re
+import html
+
 piau = re.compile('[<>*]')
 kak = re.compile('[a-z]+[0-9]\<([a-z]+[0-9])\>')
 tsenn = re.compile('[a-z]+[0-9]\*([a-z]+[0-9]-?)\*')
@@ -6,7 +8,10 @@ tsenn_khang = re.compile('[a-z]+[0-9]\*( +)\*')
 
 
 def tiau(ku):
-    hua_guan, tai_guan = ku.split('/', 1)
+    try:
+        hua_guan, tai_guan = ku.split('/', 1)
+    except ValueError:
+        hua_guan, tai_guan = '', ku
     hua = piau.sub('', hua_guan)
     tai = kak.sub(lambda m: m.group(1), tai_guan)
     tai = tsenn.sub(lambda m: m.group(1).rstrip('-'), tai)
@@ -14,3 +19,23 @@ def tiau(ku):
 
     tai = piau.sub('', tai).replace(' -', '').replace('-,', ',')
     return '{}/{}'.format(hua, tai)
+
+
+def _main():
+    with open('LTS-003.trs') as jip:
+        with open('LTS-003-hong.trs', 'w') as tshut:
+            for tsua in jip:
+                if '<' in tsua:
+                    print(tsua.rstrip(), file=tshut)
+                elif tsua.rstrip() == '':
+                    print(file=tshut)
+                else:
+                    print(
+                        html.escape(
+                            tiau(html.unescape(tsua.rstrip()))),
+                        file=tshut
+                    )
+
+
+if __name__ == '__main__':
+    _main()
