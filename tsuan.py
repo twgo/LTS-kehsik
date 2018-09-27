36cf4bc0bd10b9ce5634717a7edc9beb3cdae2a2
薛丞宏 <ihcaoe@gmail.com>
2018-09-27 10:53:56 +0800

    kah na-phing參詳好遮的情形莫處理

--- tsuan.py
+++ tsuan.py
@@ -1,10 +1,16 @@
 import re
 piau = re.compile('[<>*]')
-kak=re.compile('[a-z]+[0-9]\<([a-z]+[0-9])\>')
+kak = re.compile('[a-z]+[0-9]\<([a-z]+[0-9])\>')
+tsenn = re.compile('[a-z]+[0-9]\*([a-z]+[0-9]-?)\*')
+tsenn_khang = re.compile('[a-z]+[0-9]\*( +)\*')
+
 
 def tiau(ku):
     hua_guan, tai_guan = ku.split('/', 1)
     hua = piau.sub('', hua_guan)
-    tai=kak.sub(lambda m:m.group(1),tai_guan)
-    tai=piau.sub('',tai)
+    tai = kak.sub(lambda m: m.group(1), tai_guan)
+    tai = tsenn.sub(lambda m: m.group(1).rstrip('-'), tai)
+    tai = tsenn_khang.sub(lambda m: m.group(1), tai)
+
+    tai = piau.sub('', tai).replace(' -', '').replace('-,', ',')
     return '{}/{}'.format(hua, tai)
